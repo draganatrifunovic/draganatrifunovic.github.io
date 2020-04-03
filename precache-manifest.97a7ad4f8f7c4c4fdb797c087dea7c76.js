3a2ff282dad5b163cc18f8bf305d6abbc713ecb0
Dragana Trifunovic <gaga_trifunovic@hotmail.com>
2020-04-03 12:06:26 +0200

    added

---
+++ precache-manifest.97a7ad4f8f7c4c4fdb797c087dea7c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd09dca2edc5be19c384510d5ba40525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bca9caa1dde21ccf80c",
+    "url": "/static/css/main.d67cf834.chunk.css"
+  },
+  {
+    "revision": "59bc161ce348dde35ed5",
+    "url": "/static/js/2.11c35d78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11c35d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bca9caa1dde21ccf80c",
+    "url": "/static/js/main.622e5538.chunk.js"
+  },
+  {
+    "revision": "d102463101d6037c2913",
+    "url": "/static/js/runtime-main.10a30c17.js"
+  }
+]);
